93f1c780b943ceb624365ac312468a3a06a7e262
Andersen2311 <cahb2311@gmail.com>
2018-12-09 22:20:06 +0100

    Create scriptsheet.js

---
+++ scriptsheet.js
@@ -0,0 +1,24 @@
+function spoilerShowHide_sumregning() {
+						var x = document.getElementById("spoiler_SumLommeregner");
+						if (x.style.display === "none") {
+								x.style.display = "block";
+						} else {
+								x.style.display = "none";
+						}
+				}
+
+function sumNumbers() {
+						var num1 = (document.getElementById("n1").value);
+						var num2 = (document.getElementById("n2").value);
+						var num3 = (document.getElementById("n3").value);
+						var num4 = (document.getElementById("n4").value);
+						var num5 = (document.getElementById("n5").value);
+						var num6 = (document.getElementById("n6").value);
+						var num7 = (document.getElementById("n7").value);
+						var num8 = (document.getElementById("n8").value);
+						var num9 = (document.getElementById("n9").value);
+						var num10 = (document.getElementById("n10").value);
+
+						document.getElementById("sum_lommeregner_resultat").value=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+
+}
