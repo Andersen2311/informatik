6ba3ec1f8c5b0763951b3c1e85f1232d8125af4b
Andersen2311 <cahb2311@gmail.com>
2018-12-15 11:49:42 +0100

    Update scriptsheet.js

--- scriptsheet.js
+++ scriptsheet.js
@@ -11,14 +11,27 @@ function sumNumbers() {
 						var num1 = (document.getElementById("n1").value);
 						var num2 = (document.getElementById("n2").value);
 						var num3 = (document.getElementById("n3").value);
-						var num4 = (document.getElementById("n4").value);
-						var num5 = (document.getElementById("n5").value);
-						var num6 = (document.getElementById("n6").value);
-						var num7 = (document.getElementById("n7").value);
-						var num8 = (document.getElementById("n8").value);
-						var num9 = (document.getElementById("n9").value);
-						var num10 = (document.getElementById("n10").value);
+						//var num4 = (document.getElementById("n4").value);
+						//var num5 = (document.getElementById("n5").value);
+						//var num6 = (document.getElementById("n6").value);
+						//var num7 = (document.getElementById("n7").value);
+						//var num8 = (document.getElementById("n8").value);
+						//var num9 = (document.getElementById("n9").value);
+						//var num10 = (document.getElementById("n10").value);
 
-						document.getElementById("sum_lommeregner_resultat").value=num1+num2 ;
+						//parseFloat(n1);
+						//parseFloat(n2);
+						//parseFloat(n3);
+						//parseFloat(n4);
+						//parseFloat(n5);
+						//parseFloat(n6);
+						//parseFloat(n7);
+						//parseFloat(n8);
+						//parseFloat(n9);
+						//parseFloat(n10s);
+
+						window.alert(num1+num2+num3);
+
+						//document.getElementById("sum_lommeregner_resultat").value=num1+num2+num3;
 
 }
